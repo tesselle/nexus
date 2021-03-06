0a3c86605bd52dcb93f853b852d5d80da139e8a8
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-03-06 17:27:05 +0100

    Update DESCRIPTION

---
+++ R/nexus-package.R
@@ -0,0 +1,39 @@
+#' @details
+#' \tabular{ll}{
+#'  \strong{Package:} \tab nexus \cr
+#'  \strong{Type:} \tab Package \cr
+#'  \strong{Version:} \tab 0.0.0.9000 \cr
+#'  \strong{License:} \tab GPL-3 \cr
+#'  \strong{DOI:} \tab \doi{xxx}
+#' }
+#' @author
+#' \strong{Full list of authors and contributors} (alphabetic order)
+#'
+#' \tabular{ll}{
+#'  Nicolas Frerebeau \tab \emph{Université Bordeaux Montaigne, France} \cr
+#'  Brice Lebrun \tab \emph{Université Bordeaux Montaigne, France} \cr
+#'  Anne Philippe \tab \emph{Université de Nantes, France} \cr
+#' }
+#'
+#' \strong{Package maintainer}
+#'
+#' Nicolas Frerebeau\cr
+#' \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
+#'
+#' IRAMAT-CRP2A (UMR 5060)\cr
+#' Maison de l'Archéologie\cr
+#' Université Bordeaux Montaigne\cr
+#' F-33607 Pessac cedex\cr
+#' France
+#' @note
+#' This work received a state financial support managed by the Agence Nationale
+#' de la Recherche (France) throught the programs
+#' \emph{Investissements d'avenir}
+#' (ref. \href{https://lascarbx.labex.u-bordeaux.fr}{ANR-10-LABX-52}) and
+#' \emph{IRANGKOR} (ref.
+#' \href{https://anr.fr/Project-ANR-14-CE31-0007}{ANR-14-CE31-0007}).
+#' @name nexus-package
+#' @aliases nexus-package nexus
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
