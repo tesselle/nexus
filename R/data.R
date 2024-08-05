1d25d4c7d4cea81a2d3de9fbfd7bc8bedde3dc97
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2024-08-05 16:03:31 +0200

    Move example datasets

--- R/data.R
+++ R/data.R
@@ -57,47 +57,47 @@
 #' @keywords datasets
 "slides"
 
-#' Can Sora Petrographic Data
-#'
-#' @format A [`data.frame`] with 21 variables:
-#'  \describe{
-#'    \item{VAR1}{Optical activity.}
-#'    \item{VAR2}{Inclusion orientation.}
-#'    \item{VAR3}{Void orientation.}
-#'    \item{VAR4}{Texture.}
-#'    \item{VAR5}{Special components.}
-#'    \item{VAR6}{Plutonic rocks.}
-#'    \item{VAR7}{Volcanic rocks.}
-#'    \item{VAR8}{Metamorphic rocks.}
-#'    \item{VAR9}{Sedimentary rocks.}
-#'    \item{VAR10}{Quartz.}
-#'    \item{VAR11}{Feldspar.}
-#'    \item{VAR12}{Plagioclase.}
-#'    \item{VAR13}{Pyroxenes.}
-#'    \item{VAR14}{Amphiboles.}
-#'    \item{VAR15}{Micas.}
-#'    \item{VAR16}{Phyllosilicates.}
-#'    \item{VAR17}{Carbonates.}
-#'    \item{VAR18}{Other constituents.}
-#'    \item{VAR19}{Packing.}
-#'  }
-#' @references
-#'  Cau, M.-A., Day, P. M., Baxter, M. J., Papageorgiou, I., Iliopoulos, I. &
-#'  Montana, G. (2004). Exploring Automatic Grouping Procedures in Ceramic
-#'  Petrology. *Journal of Archaeological Science*, 31(9): 1325-1338.
-#'  \doi{10.1016/j.jas.2004.03.006}.
-#' @family datasets
-#' @keywords datasets
-"petrography"
+# Can Sora Petrographic Data
+#
+# @format A [`data.frame`] with 21 variables:
+#  \describe{
+#    \item{VAR1}{Optical activity.}
+#    \item{VAR2}{Inclusion orientation.}
+#    \item{VAR3}{Void orientation.}
+#    \item{VAR4}{Texture.}
+#    \item{VAR5}{Special components.}
+#    \item{VAR6}{Plutonic rocks.}
+#    \item{VAR7}{Volcanic rocks.}
+#    \item{VAR8}{Metamorphic rocks.}
+#    \item{VAR9}{Sedimentary rocks.}
+#    \item{VAR10}{Quartz.}
+#    \item{VAR11}{Feldspar.}
+#    \item{VAR12}{Plagioclase.}
+#    \item{VAR13}{Pyroxenes.}
+#    \item{VAR14}{Amphiboles.}
+#    \item{VAR15}{Micas.}
+#    \item{VAR16}{Phyllosilicates.}
+#    \item{VAR17}{Carbonates.}
+#    \item{VAR18}{Other constituents.}
+#    \item{VAR19}{Packing.}
+#  }
+# @references
+#  Cau, M.-A., Day, P. M., Baxter, M. J., Papageorgiou, I., Iliopoulos, I. &
+#  Montana, G. (2004). Exploring Automatic Grouping Procedures in Ceramic
+#  Petrology. *Journal of Archaeological Science*, 31(9): 1325-1338.
+#  \doi{10.1016/j.jas.2004.03.006}.
+# @family datasets
+# @keywords datasets
+#"petrography"
 
-#' Can Sora Chemical Data
-#'
-#' @format A [`data.frame`] with 30 variables.
-#' @references
-#'  Cau, M.-A. (1999). Importaciones de cerámica tardorromana de cocina en las
-#'  Iles Balears: el caso de Can Sora (Eivissa). In J. Capel Martinez,
-#'  *Arqueometria y Arqueologia*, p. 197-219. Granada: Editorial Universidad de
-#'  Granada. Monografica Arte y Arqueología 47.
-#' @family datasets
-#' @keywords datasets
-"chemistry"
+# Can Sora Chemical Data
+#
+# @format A [`data.frame`] with 30 variables.
+# @references
+#  Cau, M.-A. (1999). Importaciones de cerámica tardorromana de cocina en las
+#  Iles Balears: el caso de Can Sora (Eivissa). In J. Capel Martinez,
+#  *Arqueometria y Arqueologia*, p. 197-219. Granada: Editorial Universidad de
+#  Granada. Monografica Arte y Arqueología 47.
+# @family datasets
+# @keywords datasets
+#"chemistry"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,4 @@
-## code to prepare datasets goes here
+## Data from Aitchison (1986)
 arctic <- read.csv("data-raw/arctic.csv", header = TRUE, row.names = 1)
 usethis::use_data(arctic, overwrite = FALSE)
 
@@ -7,10 +7,3 @@ usethis::use_data(hongite, overwrite = FALSE)
 
 slides <- read.csv("data-raw/slides.csv", header = TRUE, row.names = NULL)
 usethis::use_data(slides, overwrite = FALSE)
-
-## Can Sora datasets
-chemistry <- read.csv("data-raw/cansora_chemistry.csv", header = TRUE, row.names = 1)
-usethis::use_data(chemistry, overwrite = FALSE)
-
-petrography <- read.csv("data-raw/cansora_petrography.csv", header = TRUE, row.names = 1)
-usethis::use_data(petrography, overwrite = FALSE)
